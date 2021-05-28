844fdd37ccbbfaaf221557d3dfa44c0b0fd7c58e
Tomasz Gorochowik <tgorochowik@antmicro.com>
2021-05-28 15:04:34 +0200

    Make hello formatting poor

--- hello/hello.c
+++ hello/hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(int *argc, char **argv)
+int main(
+  int *argc,
+  char **argv)
 {
-	printf("Hello world!\n");
-}
+	// comment                 
+	printf(
+			"Hello world!\n"
+		);
+	return 0; }
